d0bf7252b839b0ec8dae58ce7260c1c141c8b5b7
hussain <nasiba.ansari@gmail.com>
2020-03-18 15:04:08 +0530

    Add To Home Screen

--- js/main.js
+++ js/main.js
@@ -1,32 +1,68 @@
 (function() {
-  const header = document.querySelector("header");
-  const menu = document.querySelector(".header__hamburger");
-  const nav = document.querySelector(".nav");
-  const nav__overlay = document.querySelector(".nav__overlay");
+  const header = document.querySelector('header');
+  const menu = document.querySelector('.header__hamburger');
+  const nav = document.querySelector('.nav');
+  const nav__overlay = document.querySelector('.nav__overlay');
+  const addToHome = document.querySelector('.addToHome');
+  const addBtn = document.querySelector('.btn__addToHome');
+  const closeBtn = document.querySelector('.addToHome__close');
+
+  addBtn.style.display = 'none';
+  let deferredPrompt;
+
+  window.addEventListener('beforeinstallprompt', e => {
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e;
+    // Update UI to notify the user they can add to home screen
+    addBtn.style.display = 'block';
+
+    addBtn.addEventListener('click', e => {
+      // hide our user interface that shows our A2HS button
+      addBtn.style.display = 'none';
+      // Show the prompt
+      deferredPrompt.prompt();
+      // Wait for the user to respond to the prompt
+      deferredPrompt.userChoice.then(choiceResult => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt');
+        } else {
+          console.log('User dismissed the A2HS prompt');
+        }
+        deferredPrompt = null;
+      });
+    });
+
+    closeBtn.addEventListener('click', e => {
+      e.preventDefault();
+      addToHome.style.display = 'none';
+    });
+  });
 
   const stickyHeader = () => {
     if (
       document.body.scrollTop > 50 ||
       document.documentElement.scrollTop > 50
     ) {
-      header.className = "header sticky";
+      header.className = 'header sticky';
     } else {
-      header.className = "header";
+      header.className = 'header';
     }
   };
 
   const toggleMenu = () => {
-    if (menu.className === "header__hamburger") {
-      menu.className = "header__hamburger header__hamburger--show";
-      nav.className = "nav nav--show";
-      nav__overlay.className = "nav__overlay nav__overlay--show";
+    if (menu.className === 'header__hamburger') {
+      menu.className = 'header__hamburger header__hamburger--show';
+      nav.className = 'nav nav--show';
+      nav__overlay.className = 'nav__overlay nav__overlay--show';
     } else {
-      menu.className = "header__hamburger";
-      nav.className = "nav";
-      nav__overlay.className = "nav__overlay";
+      menu.className = 'header__hamburger';
+      nav.className = 'nav';
+      nav__overlay.className = 'nav__overlay';
     }
   };
 
-  menu.addEventListener("click", toggleMenu);
-  window.addEventListener("scroll", stickyHeader);
+  menu.addEventListener('click', toggleMenu);
+  window.addEventListener('scroll', stickyHeader);
 })();
