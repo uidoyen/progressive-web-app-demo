ff7265123aa3a1c584437aacf6bd7eaaa7668599
hussain <nasiba.ansari@gmail.com>
2020-03-18 15:53:22 +0530

    Add to Home Screen

--- js/main.js
+++ js/main.js
@@ -7,38 +7,34 @@
   const addBtn = document.querySelector('.btn__addToHome');
   const closeBtn = document.querySelector('.addToHome__close');
 
-  addBtn.style.display = 'none';
   let deferredPrompt;
-
-  window.addEventListener('beforeinstallprompt', e => {
+  window.addEventListener('beforeinstallprompt', function(e) {
     console.log('beforeinstallprompt');
     // Prevent Chrome 67 and earlier from automatically showing the prompt
     e.preventDefault();
     // Stash the event so it can be triggered later.
     deferredPrompt = e;
-    // Update UI to notify the user they can add to home screen
-    addBtn.style.display = 'block';
+  });
 
-    addBtn.addEventListener('click', e => {
-      // hide our user interface that shows our A2HS button
-      addBtn.style.display = 'none';
-      // Show the prompt
-      deferredPrompt.prompt();
-      // Wait for the user to respond to the prompt
-      deferredPrompt.userChoice.then(choiceResult => {
-        if (choiceResult.outcome === 'accepted') {
-          console.log('User accepted the A2HS prompt');
-        } else {
-          console.log('User dismissed the A2HS prompt');
-        }
-        deferredPrompt = null;
-      });
+  addBtn.addEventListener('click', e => {
+    // hide our user interface that shows our A2HS button
+    addToHome.style.display = 'none';
+    // Show the prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then(choiceResult => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt');
+      } else {
+        console.log('User dismissed the A2HS prompt');
+      }
+      deferredPrompt = null;
     });
+  });
 
-    closeBtn.addEventListener('click', e => {
-      e.preventDefault();
-      addToHome.style.display = 'none';
-    });
+  closeBtn.addEventListener('click', e => {
+    e.preventDefault();
+    addToHome.style.display = 'none';
   });
 
   const stickyHeader = () => {
