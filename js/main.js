ae405cf2283be7c2b4c495cdae051ba175865c0f
hussain <nasiba.ansari@gmail.com>
2020-03-18 15:08:50 +0530

    Add To Home Screen

--- js/main.js
+++ js/main.js
@@ -11,6 +11,7 @@
   let deferredPrompt;
 
   window.addEventListener('beforeinstallprompt', e => {
+    console.log('beforeinstallprompt');
     // Prevent Chrome 67 and earlier from automatically showing the prompt
     e.preventDefault();
     // Stash the event so it can be triggered later.
